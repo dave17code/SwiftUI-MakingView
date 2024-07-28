cc7e16faa92d2beea34e0282debf2f6ede1a8293
dave17code <dave17code@gmail.com>
2024-07-28 20:10:25 +0900

    Initial Commit

---
+++ MakingView/MakingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakingViewApp.swift
+//  MakingView
+//
+//  Created by 김두운 on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MakingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
